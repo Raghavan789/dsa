3491cb7675c8e7b6cd2c7c33163d00e9f9ec0d52
Raghavan789 <mavkrishnaraghavan@gmail.com>
2024-05-29 16:26:32 +0530

    did daytwoincreasing

--- daywise-programs/cses/daytwoincreasing.java
+++ daywise-programs/cses/daytwoincreasing.java
@@ -1,28 +1,26 @@
-import java.util.*;
+import java.util.Scanner;
+
 public class daytwoincreasing {
 
-    public static int minMovesIncreasing(int[] arr) {
-        int n = arr.length;
-        int moves = 0;
-        int prev = Integer.MIN_VALUE;  
-        for (int num : arr) {
-            // Similar logic as the previous versions
-            moves += Math.max(0, num - prev);
-            prev = num;
-        }
-
-        return moves;
-    }
-
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-	int n=sc.nextInt();
-	int[] arr= new int[n];
-	for(int i=0;i<n;i++)
-	{
-		arr[i]=sc.nextInt();
-	}
-        int minMoves = minMovesIncreasing(arr);
-        System.out.println("Minimum moves required: " + minMoves);
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+        long[] array = new long[n];
+        
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextLong();
+        }
+        
+        long moves = 0;
+        
+        for (int i = 1; i < n; i++) {
+            if (array[i] < array[i - 1]) {
+                moves += array[i - 1] - array[i];
+                array[i] = array[i - 1];
+            }
+        }
+        
+        System.out.println(moves);
     }
 }
